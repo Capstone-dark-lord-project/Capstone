a5bb25b707e58de31990ff2213bca068b784c3e8
Intouch Yuso <act22117@gmail.com>
2024-02-26 22:57:29 +0700

    Drag and drop

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class DragAndDrop : MonoBehaviour
+ 
+public class DraggableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody _rigidbody;
+    private float _startYPos;
+    private TableController _board;
+    
     void Start()
     {
-        
+        _board = FindObjectOfType<TableController>();
+        _rigidbody = GetComponent<Rigidbody>();
+ 
+        _startYPos = 0; // Better to not hardcode that one but whatever
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    private void OnMouseDrag()
     {
-        
+        Vector3 newWorldPosition = new Vector3(_board.CurrentMousePosition.x, _startYPos + 1, _board.CurrentMousePosition.z);
+ 
+        var difference = newWorldPosition - transform.position;
+ 
+        var speed = 10 * difference;
+        _rigidbody.velocity = speed;
+        _rigidbody.rotation = Quaternion.Euler(new Vector3(speed.z, 0, -speed.x));
     }
 }
